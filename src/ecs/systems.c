a6ca00afe13e1fe4a71adcb3ca27e1093eae9e69
philip <philster36@hotmail.co.uk>
2024-10-14 13:54:11 +0100

    created system runner

---
+++ src/ecs/systems.c
@@ -0,0 +1,52 @@
+#include "systems.h"
+#include "world.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void system_runner_alloc(SystemRunner *sysRunner) {
+    sysRunner->startupSystemCount = 0;
+    sysRunner->startupSystems = (SystemPtr *)malloc(sizeof(SystemPtr));
+    sysRunner->updateSystemCount = 0;
+    sysRunner->updateSystems = (SystemPtr *)malloc(sizeof(SystemPtr));
+}
+
+void system_runner_add_system(SystemRunner *sysRunner, World *world,
+                              SystemSet set, SystemPtr sys) {
+
+    if (strcmp(set, SYSTEM_SET_STARTUP) == 0) {
+        // Allocates more space for the startup systems
+        sysRunner->startupSystemCount++;
+        sysRunner->startupSystems = (SystemPtr *)realloc(
+            sysRunner->startupSystems,
+            sysRunner->startupSystemCount * sizeof(SystemPtr));
+
+        sysRunner->startupSystems[sysRunner->startupSystemCount - 1] = sys;
+    }
+
+    if (strcmp(set, SYSTEM_SET_UPDATE) == 0) {
+        // Allocates more space for the update systems
+        sysRunner->updateSystemCount++;
+        sysRunner->updateSystems = (SystemPtr *)realloc(
+            sysRunner->updateSystems,
+            sysRunner->updateSystemCount * sizeof(SystemPtr));
+        sysRunner->updateSystems[sysRunner->updateSystemCount - 1] = sys;
+    }
+}
+
+void system_runner_run_startup_systems(SystemRunner *sysRunner, World *world) {
+    for (size_t i = 0; i < sysRunner->startupSystemCount; i++) {
+        sysRunner->startupSystems[i](world);
+    }
+}
+
+void system_runner_run_update_systems(SystemRunner *sysRunner, World *world) {
+    for (size_t i = 0; i < sysRunner->updateSystemCount; i++) {
+        sysRunner->updateSystems[i](world);
+    }
+}
+
+void system_runner_free(SystemRunner *sysRunner) {
+    free(sysRunner->startupSystems);
+    free(sysRunner->updateSystems);
+}
---
+++ src/ecs/systems.h
@@ -0,0 +1,27 @@
+#ifndef _SYSTEMS_HEADER_H
+#define _SYSTEMS_HEADER_H
+
+#include "query.h"
+#include "world.h"
+
+typedef char* SystemSet;
+
+typedef void (*SystemPtr)(World* world);
+typedef struct {
+    size_t startupSystemCount;
+    SystemPtr* startupSystems;
+    size_t updateSystemCount;
+    SystemPtr* updateSystems;
+} SystemRunner;
+
+
+void system_runner_alloc(SystemRunner *sysRunner);
+void system_runner_add_system(SystemRunner* ecs, World* world, SystemSet set, SystemPtr sys);
+void system_runner_run_startup_systems(SystemRunner* sysRunner, World* world);
+void system_runner_run_update_systems(SystemRunner* sysRunner, World* world);
+void system_runner_free(SystemRunner *sysRunner);
+
+#define SYSTEM_SET_STARTUP "system_set::startup"
+#define SYSTEM_SET_UPDATE "system_set::update"
+
+#endif // !_SYSTEMS_HEADER_H
