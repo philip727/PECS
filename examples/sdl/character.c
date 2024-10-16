09e8710b6fe90274c12ffcedab442fa9917fb1fb
philip <philster36@hotmail.co.uk>
2024-10-16 23:09:33 +0100

    speed component

--- examples/sdl/character.c
+++ examples/sdl/character.c
@@ -8,10 +8,14 @@
 #include "character.h"
 #include "world.h"
 #include "draw.h"
+#include "time.h"
 #include "app.h"
 #include "SDL3/SDL_rect.h"
+#include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 void spawn_character_sys(World *world) {
     size_t id = world_create_empty_entity(world);
@@ -44,6 +48,13 @@ void spawn_character_sys(World *world) {
     transformComponent.type = TRANSFORM_COMPONENT_TYPE;
     transformComponent.data = (void *)transform;
     world_insert_component(world, id, transformComponent);
+
+    ComponentData speedComponent = {};
+    speedComponent.type = SPEED_COMPONENT_TYPE;
+    CSpeed *speed = (CSpeed *)malloc(sizeof(CSpeed));
+    speed->value = 500;
+    speedComponent.data = (void *)speed;
+    world_insert_component(world, id, speedComponent);
 }
 
 void move_character_sys(World *world) {
@@ -51,10 +62,14 @@ void move_character_sys(World *world) {
         world_get_resource(world, SDL_CTX_RESOURCE_TYPE);
     RSDLContext *sdlCtx = (RSDLContext *)sdlCtxResource->data;
 
+    ResourceData *timeResource = world_get_resource(world, TIME_RESOURCE_TYPE);
+    RTime *time = (RTime *)timeResource->data;
+
     // Marked with local character component, so we dont move other transforms.
-    char *types[] = {LOCAL_CHARACTER_COMPONENT_TYPE, TRANSFORM_COMPONENT_TYPE};
+    char *types[] = {LOCAL_CHARACTER_COMPONENT_TYPE, TRANSFORM_COMPONENT_TYPE,
+                     SPEED_COMPONENT_TYPE};
     Query query = {
-        .amountOfComponents = 2,
+        .amountOfComponents = 3,
         .componentTypes = types,
     };
 
@@ -68,22 +83,31 @@ void move_character_sys(World *world) {
     QueryResultEntry entry = result.entries[0];
     // Should be the second component as local character is first in query
     CTransform *transform = (CTransform *)entry.components[1]->data;
+    // Get the speed set in the speed component
+    CSpeed *speed = (CSpeed *)entry.components[2]->data;
 
+    double deltaSeconds = time_get_delta_seconds(time);
     const bool *keyboardState = SDL_GetKeyboardState(NULL);
-
+    Vec2 movementDir = {.x = 0, .y = 0};
     if (keyboardState[SDL_SCANCODE_W]) {
-        transform->position.y -= 1;
+        movementDir.y -= 1;
     }
 
     if (keyboardState[SDL_SCANCODE_S]) {
-        transform->position.y += 1;
+        movementDir.y += 1;
     }
 
     if (keyboardState[SDL_SCANCODE_D]) {
-        transform->position.x += 1;
+        movementDir.x += 1;
     }
 
     if (keyboardState[SDL_SCANCODE_A]) {
-        transform->position.x -= 1;
+        movementDir.x -= 1;
     }
+
+    transform->position =
+        vec2_add(transform->position,
+                 vec2_mul_double(
+                     vec2_mul_double(vec2_normalize(movementDir), speed->value),
+                     deltaSeconds));
 }
--- examples/sdl/character.h
+++ examples/sdl/character.h
@@ -1,5 +1,10 @@
 #include "world.h"
 
 #define LOCAL_CHARACTER_COMPONENT_TYPE "component::local"
+#define SPEED_COMPONENT_TYPE "component::speed"
+typedef struct {
+    float value;
+} CSpeed;
+
 void spawn_character_sys(World* world);
 void move_character_sys(World *world);
